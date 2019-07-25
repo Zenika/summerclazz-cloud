04911e2095694d771b945696c6c5102ce2eba307
Pierre-Yves Aillet <pyaillet@gmail.com>
2019-07-25 13:53:27 +0200

    :pencil: adding hadolint rules ref

--- ci/lint.sh
+++ ci/lint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# See rules here: https://github.com/opencontainers/image-spec/blob/v1.0.0/annotations.md#pre-defined-annotation-keys
+
 docker run \
   --rm \
   -v $(pwd)/Dockerfile:/usr/src/app/Dockerfile:ro \
