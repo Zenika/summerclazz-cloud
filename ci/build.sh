62c0fb11e2897751caf87dda6f22bce446501754
Pierre-Yves Aillet <pyaillet@gmail.com>
2019-07-26 09:05:58 +0200

    :sparkles: supporting build profiles

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-DOCKERFILE=Dockerfile.${BUILD_PROFILE:-jvm}
+if [[ ! -z $BUILD_PROFILE ]]; then
+  DOCKERFILE=Dockerfile.$BUILD_PROFILE
+else
+  DOCKERFILE=Dockerfile.jvm
+fi
 
+echo Using profile $BUILD_PROFILE
 docker build . \
   -f $DOCKERFILE \
   -t pyaillet/summerclazz
