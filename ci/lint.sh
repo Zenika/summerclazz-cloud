c035a49d05e42efbfde6dd7c63fd3d54c3b011fb
Pierre-Yves Aillet <pyaillet@gmail.com>
2019-07-26 08:42:22 +0200

    :construction_worker: adding lint on multiprofiles

--- ci/lint.sh
+++ ci/lint.sh
@@ -2,8 +2,16 @@
 
 # See rules here: https://github.com/opencontainers/image-spec/blob/v1.0.0/annotations.md#pre-defined-annotation-keys
 
-docker run \
-  --rm \
-  -v $(pwd)/Dockerfile:/usr/src/app/Dockerfile:ro \
-  -i hadolint/hadolint \
-  hadolint /usr/src/app/Dockerfile
+
+check_dockerfile() {
+  DOCKERFILE=$1
+
+  docker run \
+    --rm \
+    -v $(pwd)/$DOCKERFILE:/usr/src/app/Dockerfile:ro \
+    -i hadolint/hadolint \
+    hadolint /usr/src/app/Dockerfile
+}
+
+check_dockerfile Dockerfile.jvm
+check_dockerfile Dockerfile.native
